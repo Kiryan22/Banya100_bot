f08dbca773565906a9c2f4abbd7a2cdd15f566ef
Shamanskyi Kyryl <shaman.kiryan@gmail.com>
2025-05-12 10:56:35 +0200

    Исправлены импорты и регистрация команд: убраны несуществующие обработчики (handle_cash_payment, history, visits, stats)

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from database import Database
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 
 # Импорт обработчиков
-from handlers.bath import register_bath, create_bath_event, button_callback, confirm_bath_registration, handle_payment_confirmation, admin_confirm_payment, admin_decline_payment, handle_cash_payment, handle_deep_link
+from handlers.bath import register_bath, create_bath_event, button_callback, confirm_bath_registration, handle_payment_confirmation, admin_confirm_payment, admin_decline_payment, handle_deep_link
 from handlers.profile import profile, handle_profile_update, handle_full_name, handle_birth_date, handle_occupation, handle_instagram, handle_skills, start_profile_callback, export_profiles, cancel
-from handlers.admin import mark_paid, add_subscriber, remove_subscriber, update_commands, export_profiles, mention_all, stats, mark_visit, clear_db
+from handlers.admin import mark_paid, add_subscriber, remove_subscriber, update_commands, export_profiles, mention_all, mark_visit, clear_db
 
 logger = get_logger(__name__)
 db = Database()
@@ -20,8 +20,6 @@ if __name__ == "__main__":
     application.add_handler(CommandHandler("register", register_bath))
     application.add_handler(CommandHandler("create_bath", create_bath_event))
     application.add_handler(CommandHandler("profile", profile))
-    application.add_handler(CommandHandler("history", stats))
-    application.add_handler(CommandHandler("visits", stats))
     application.add_handler(CommandHandler("cash_list", admin_confirm_payment))
     application.add_handler(CommandHandler("mark_paid", mark_paid))
     application.add_handler(CommandHandler("add_subscriber", add_subscriber))
@@ -29,7 +27,6 @@ if __name__ == "__main__":
     application.add_handler(CommandHandler("update_commands", update_commands))
     application.add_handler(CommandHandler("export_profiles", export_profiles))
     application.add_handler(CommandHandler("mention_all", mention_all))
-    application.add_handler(CommandHandler("stats", stats))
     application.add_handler(CommandHandler("mark_visit", mark_visit))
     application.add_handler(CommandHandler("clear_db", clear_db))
 
@@ -37,7 +34,6 @@ if __name__ == "__main__":
     application.add_handler(CallbackQueryHandler(button_callback))
     application.add_handler(CallbackQueryHandler(confirm_bath_registration, pattern="^confirm_bath_"))
     application.add_handler(CallbackQueryHandler(handle_payment_confirmation, pattern="^paid_bath_"))
-    application.add_handler(CallbackQueryHandler(handle_cash_payment, pattern="^cash_bath_"))
     application.add_handler(CallbackQueryHandler(admin_confirm_payment, pattern="^admin_confirm_"))
     application.add_handler(CallbackQueryHandler(admin_decline_payment, pattern="^admin_decline_"))
 
